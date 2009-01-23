53d9a7629c40ff15854e8fb28a3913224451ad9b
ruslanas.com <ruslanas.com@5b469e08-e575-11dd-bd7b-b12b6590754f>
2009-01-23 02:33:46 +0000

    git-svn-id: http://bitly.googlecode.com/svn/trunk@17 5b469e08-e575-11dd-bd7b-b12b6590754f

--- jquery.shortenurl.js
+++ jquery.shortenurl.js
@@ -4,17 +4,44 @@ URL: http://code.ruslanas.com
 jQuery plugin for url shortening uses bit.ly API
 */
 
-jQuery.fn.shortenUrl = function(url) {
-    if( !url) {
-        return false;
-    }
+jQuery.fn.shorten = function(func) {
+    return this.each( function() {
+        var elm = jQuery(this);
+        jQuery.post('shorten.php', {
+            'action' : 'shorten',
+            'url' : elm.attr('href')
+        }, func, 'json');
+    });
+}
+
+jQuery.fn.stats = function(func) {
+    return this.each( function() {
+        var elm = jQuery(this);
+        jQuery.post('shorten.php', {
+            'action' : 'stats',
+            'url' : elm.attr('href')
+        }, func, 'json');
+    });
+}
+
+jQuery.fn.info = function(func) {
+    return this.each( function() {
+        var elm = jQuery(this);
+        jQuery.post('shorten.php', {
+            'action' : 'info',
+            'url' : elm.attr('href')
+        }, func, 'json');
+    });
+}
+
+jQuery.fn.shortenUrl = function() {
     return this.each(function(){
         var elm = jQuery(this);
         var long = elm.val();
         if( !long) {
             return false;
         }
-        jQuery.post(url, {
+        jQuery.post('shorten.php', {
             'action' : 'shorten',
             'url' : long
         }, function(data, status) {
