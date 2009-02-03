443ccfb96ec489c3ce7fd02839b0b6a454cc8c6f
ruslanas.com <ruslanas.com@5b469e08-e575-11dd-bd7b-b12b6590754f>
2009-02-03 14:30:14 +0000

    Simple SOAP server and test client added.
    git-svn-id: http://bitly.googlecode.com/svn/trunk@44 5b469e08-e575-11dd-bd7b-b12b6590754f

---
+++ soap.php
@@ -0,0 +1,17 @@
+<?php
+ini_set("soap.wsdl_cache_enabled", "0");
+include('lib/bitly.class.php');
+
+class BitlyService { 
+    
+    function getShortened($url) {
+        include('config.php');
+        $bitly = new Bitly($login, $apiKey);
+        return $bitly->shortenSingle($url);
+    }
+}
+
+$server = new SoapServer("http://ruslanas.com/bitly/bitly.wsdl");
+$server->setClass("BitlyService");
+$server->handle();
+?>
